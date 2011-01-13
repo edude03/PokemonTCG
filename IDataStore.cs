d9ea236f1b3e3246a43c296e6ceb464e9174fdf2
Michael Francis <edude03@gmail.com>
2011-01-12 21:28:20 -0500

    Implemented a datastore interface (for switch databases at will)

---
+++ IDataStore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PokemonTCG
+{
+    public interface IDataStore
+    {
+        Card getCardbyID(int BOGUS_ID);
+        List<Card> getCardArray(int[] intArray);
+        
+        //I guess?
+        void connect();
+        void disconnect(); 
+    }
+}
